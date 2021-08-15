7e284fa498af64c546db659a87e86b3be12f1a2e
Dominic Poku-Aidoo <dominic.aidoo@gmail.com>
2021-08-15 10:30:28 -0400

    Refactoring

--- src/services/crypto.service.ts
+++ src/services/crypto.service.ts
@@ -1,15 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { map } from 'rxjs';
-import {_} from 'lodash';
+import { map, Observable } from 'rxjs';
+import { CryptoAssetResponse } from 'src/interfaces/Interfaces';
 
 
 @Injectable()
 export class CryptoService {
     constructor(private httpService: HttpService) {}
 
-    getCryptoPrice(cryptoAsset: string): any {
+    getCryptoPrice(cryptoAsset: string): Observable<CryptoAssetResponse> {
         let coinGeckoUrl = 'https://api.coingecko.com/api/v3/coins/';
+        let cryptoAssetResp;
+
         if(cryptoAsset == 'BTC')
             coinGeckoUrl = coinGeckoUrl + 'bitcoin';
         if(cryptoAsset == 'ETH')
@@ -17,16 +19,15 @@ export class CryptoService {
 
         return this.httpService.get(coinGeckoUrl).pipe(
             map(response => {
-                let assetValue = response.data.market_data.current_price.usd.toFixed(1);
-                console.log("pricee:: %s for asset::: %s",assetValue, cryptoAsset);
-                return {"assetId": cryptoAsset, "value": assetValue };
+                if (response.status.valueOf() === 200){
+                    let assetValue = response.data.market_data.current_price.usd.toFixed(1);
+                    console.log("price:: %s for asset::: %s",assetValue, cryptoAsset);
+                    cryptoAssetResp = new CryptoAssetResponse(cryptoAsset,assetValue);
+                    console.log(cryptoAssetResp);
+                    return cryptoAssetResp;
+                }
             })
         );
 
     }
-}
-
-function round(value, precision) {
-    var multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
 }
